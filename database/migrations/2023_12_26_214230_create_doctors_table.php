6afa5a0ce85a90b38f0a98accc13ac9ce44eb9b5
athraz <athrazan2004@gmail.com>
2023-12-27 05:38:33 +0700

    feat: migrations

---
+++ database/migrations/2023_12_26_214230_create_doctors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('gender');
+            $table->integer('age');
+            $table->foreignId('admin_id')->references('id')->on('admins')->onDelete('cascade')->constrained()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
---
+++ database/migrations/2023_12_26_214303_create_assessments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_verified');
+            $table->longText('notes')->nullable();
+            $table->foreignId('admin_id')->references('id')->on('admins')->onDelete('cascade')->constrained()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessments');
+    }
+};

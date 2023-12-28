805bbafe35a58fc67fdfbb250098fcdfd8771b41
aptarr_ <apta.rasendriya@gmail.com>
2023-12-28 16:59:01 +0700

    fix: radio red

--- resources/views/assessment/assesment_question.blade.php
+++ resources/views/assessment/assesment_question.blade.php
@@ -15,25 +15,60 @@
                 <div class="flex justify-between items-center">
                     <div class="flex flex-wrap items-center">
                         <h1 class="font-bold me-8 max-md:me-2">Min</h1>
-                        <div class="flex items-center flex-col me-8 max-md:me-2">
-                            <input id="red-radio" type="radio" value="1" name="colored-radio" class="w-16 h-16 max-md:w-10 max-md:h-10 max bg-gray-100 border-gray-300  focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="red-radio" class="mt-1 text-sm font-medium text-gray-900 dark:text-gray-500">1</label>
+                        <div class="flex items-center flex-col me-8 max-md:me-2 relative">
+                            <input id="red" name="color" type="radio" class="before:content[''] peer relative w-16 h-16 max-md:w-10 max-md:h-10 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-red-600 transition-all before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-y-1/2 before:-translate-x-1/2 before:block before:h-12 before:w-12 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:before:bg-red-500 hover:before:opacity-10" />
+                            <span class="absolute text-red-500 transition-opacity opacity-0 pointer-events-none top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 max-md:w-10 max-md:h-10">
+                                    <circle cx="50%" cy="50%" r="50%" fill="currentColor" />
+                                </svg>
+                            </span>
+                            <label for="red" class="mt-1 text-sm font-medium text-red-600 dark:text-gray-500 absolute top-full left-1/2 transform -translate-x-1/2">
+                                1
+                            </label>
                         </div>
-                        <div class="flex items-center flex-col me-8 max-md:me-2">
-                            <input id="red-radio" type="radio" value="2" name="colored-radio" class="w-16 h-16 max-md:w-10 max-md:h-10 max bg-gray-100 border-gray-300  focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="red-radio" class="mt-1 text-sm font-medium text-gray-900 dark:text-gray-500">2</label>
+                        <div class="flex items-center flex-col me-8 max-md:me-2 relative">
+                            <input id="red" name="color" type="radio" class="before:content[''] peer relative w-16 h-16 max-md:w-10 max-md:h-10 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-red-600 transition-all before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-y-1/2 before:-translate-x-1/2 before:block before:h-12 before:w-12 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:before:bg-red-500 hover:before:opacity-10" />
+                            <span class="absolute text-red-500 transition-opacity opacity-0 pointer-events-none top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 max-md:w-10 max-md:h-10">
+                                    <circle cx="50%" cy="50%" r="50%" fill="currentColor" />
+                                </svg>
+                            </span>
+                            <label for="red" class="mt-1 text-sm font-medium text-red-600 dark:text-gray-500 absolute top-full left-1/2 transform -translate-x-1/2">
+                                2
+                            </label>
                         </div>
-                        <div class="flex items-center flex-col me-8 max-md:me-2">
-                            <input id="red-radio" type="radio" value="3" name="colored-radio" class="w-16 h-16 max-md:w-10 max-md:h-10 max bg-gray-100 border-gray-300  focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="red-radio" class="mt-1 text-sm font-medium text-gray-900 dark:text-gray-500">3</label>
+                        <div class="flex items-center flex-col me-8 max-md:me-2 relative">
+                            <input id="red" name="color" type="radio" class="before:content[''] peer relative w-16 h-16 max-md:w-10 max-md:h-10 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-red-600 transition-all before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-y-1/2 before:-translate-x-1/2 before:block before:h-12 before:w-12 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:before:bg-red-500 hover:before:opacity-10" />
+                            <span class="absolute text-red-500 transition-opacity opacity-0 pointer-events-none top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 max-md:w-10 max-md:h-10">
+                                    <circle cx="50%" cy="50%" r="50%" fill="currentColor" />
+                                </svg>
+                            </span>
+                            <label for="red" class="mt-1 text-sm font-medium text-red-600 dark:text-gray-500 absolute top-full left-1/2 transform -translate-x-1/2">
+                                3
+                            </label>
                         </div>
-                        <div class="flex items-center flex-col me-8 max-md:me-2">
-                            <input id="red-radio" type="radio" value="4" name="colored-radio" class="w-16 h-16 max-md:w-10 max-md:h-10 max bg-gray-100 border-gray-300  focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="red-radio" class="mt-1 text-sm font-medium text-gray-900 dark:text-gray-500">4</label>
+                        <div class="flex items-center flex-col me-8 max-md:me-2 relative">
+                            <input id="red" name="color" type="radio" class="before:content[''] peer relative w-16 h-16 max-md:w-10 max-md:h-10 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-red-600 transition-all before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-y-1/2 before:-translate-x-1/2 before:block before:h-12 before:w-12 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:before:bg-red-500 hover:before:opacity-10" />
+                            <span class="absolute text-red-500 transition-opacity opacity-0 pointer-events-none top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 max-md:w-10 max-md:h-10">
+                                    <circle cx="50%" cy="50%" r="50%" fill="currentColor" />
+                                </svg>
+                            </span>
+                            <label for="red" class="mt-1 text-sm font-medium text-red-600 dark:text-gray-500 absolute top-full left-1/2 transform -translate-x-1/2">
+                                4
+                            </label>
                         </div>
-                        <div class="flex items-center flex-col me-8 max-md:me-2">
-                            <input id="red-radio" type="radio" value="5" name="colored-radio" class="w-16 h-16 max-md:w-10 max-md:h-10 max bg-gray-100 border-gray-300  focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="red-radio" class="mt-1 text-sm font-medium text-gray-900 dark:text-gray-500">5</label>
+                        <div class="flex items-center flex-col me-8 max-md:me-2 relative">
+                            <input id="red" name="color" type="radio" class="before:content[''] peer relative w-16 h-16 max-md:w-10 max-md:h-10 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-red-600 transition-all before:absolute before:top-1/2 before:left-1/2 before:transform before:-translate-y-1/2 before:-translate-x-1/2 before:block before:h-12 before:w-12 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-red-500 checked:before:bg-red-500 hover:before:opacity-10" />
+                            <span class="absolute text-red-500 transition-opacity opacity-0 pointer-events-none top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 peer-checked:opacity-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 max-md:w-10 max-md:h-10">
+                                    <circle cx="50%" cy="50%" r="50%" fill="currentColor" />
+                                </svg>
+                            </span>
+                            <label for="red" class="mt-1 text-sm font-medium text-red-600 dark:text-gray-500 absolute top-full left-1/2 transform -translate-x-1/2">
+                                5
+                            </label>
                         </div>
                         <h1 class="font-bold">Max</h1>
                     </div>

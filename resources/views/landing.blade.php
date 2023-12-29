dfa58e6f79a12ab4f2ec40165a2da440b882e768
kecoaktempur <triwan98@gmail.com>
2023-12-29 22:13:14 +0700

    landing fixing done

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -73,7 +73,7 @@
                 </div>
                 <div class="tab-2-content-bottom">
                     <div>
-                        <img src="https://i.ibb.co/DpdN7Gn/tab-content-2-1.png" alt="" />
+                        <img src="img/ipad.png" alt="" />
                         <p class="text-md">
                             Consult Anywhere, Anytime: Your Health, Your Way!
                         </p>
@@ -85,7 +85,7 @@
                         </p>
                     </div>
                     <div>
-                        <img src="https://i.ibb.co/R3r1SPX/tab-content-2-2.png" alt="" />
+                        <img style="margin-top: 40px;margin-bottom: 42px;" src="img/macbook.png" alt="" />
                         <p class="text-md">
                             Flexible Scheduling: Your Time, Your Terms
                         </p>
@@ -94,7 +94,7 @@
                         </p>
                     </div>
                     <div>
-                        <img src="https://i.ibb.co/gDhnwWn/tab-content-2-3.png" alt="" />
+                        <img src="img/mac.png" alt="" />
                         <p class="text-md">
                             Personalized Care, Wherever You Are
                         </p>
@@ -110,68 +110,36 @@
             <div id="tab-3-content" class="tab-content-item">
                 <div class="text-center">
                     <p class="text-lg">
-                        Based on web and mobile platform, our system is accessible from anywhere.
+                        24/7 Doctor Ready: Your Health, Anytime You Need
                     </p>
-                    <a href="#" class="btn btn-lg">Watch Free For 30 Days</a>
                 </div>
 
                 <table class="table">
                     <thead>
                         <tr>
-                            <th></th>
-                            <th>Basic</th>
-                            <th>Standard</th>
-                            <th>Premium</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>Monthly price after free month ends on 6/19/19</td>
-                            <td>$8.99</td>
-                            <td>$12.99</td>
-                            <td>$15.99</td>
+                            <td>Immediate Access to Care: Day or Night</td>
                         </tr>
                         <tr>
                             <td>HD Available</td>
-                            <td><i class="fas fa-times"></i></td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
                         </tr>
                         <tr>
-                            <td>Ultra HD Available</td>
-                            <td><i class="fas fa-times"></i></td>
-                            <td><i class="fas fa-times"></i></td>
-                            <td><i class="fas fa-check"></i></td>
+                            <td>Virtual Consultations Anywhere, Anytime</td>
                         </tr>
                         <tr>
-                            <td>Screens you can watch on at the same time</td>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>4</td>
+                            <td>Your Health, Our Priority - Always</td>
                         </tr>
                         <tr>
-                            <td>Watch on your laptop, TV, phone and tablet</td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
+                            <td>Prompt and Comprehensive Care</td>
                         </tr>
                         <tr>
-                            <td>Unlimited movies and TV shows</td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
+                            <td>Patient-Centric Approach</td>
                         </tr>
                         <tr>
-                            <td>Cancel anytime</td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
-                        </tr>
-                        <tr>
-                            <td>First month free</td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
-                            <td><i class="fas fa-check"></i></td>
+                            <td>Leading Specialists</td>
                         </tr>
                     </tbody>
                 </table>
@@ -180,35 +148,9 @@
     </section>
 
     <footer class="footer">
-        <p>Questions? Call 1-866-579-7172</p>
-        <div class="footer-cols">
-            <ul>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Investor Relations</a></li>
-                <li><a href="#">Ways To Watch</a></li>
-                <li><a href="#">Corporate Information</a></li>
-                <li><a href="#">Netflix Originals</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">Help Center</a></li>
-                <li><a href="#">Jobs</a></li>
-                <li><a href="#">Terms Of Use</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">Account</a></li>
-                <li><a href="#">Redeem Gift Cards</a></li>
-                <li><a href="#">Privacy</a></li>
-                <li><a href="#">Speed Test</a></li>
-            </ul>
-            <ul>
-                <li><a href="#">Media Center</a></li>
-                <li><a href="#">Buy Gift Cards</a></li>
-                <li><a href="#">Cookie Preferences</a></li>
-                <li><a href="#">Legal Notices</a></li>
-            </ul>
+        <div class="footer-content">
+            <img src="img/logo-footer.png" alt="">
         </div>
-
     </footer>
     <script src="js/landing.js"></script>
 </body>

45ebc37ef6ad7287398f70a8125579293bd48285
kecoaktempur <triwan98@gmail.com>
2023-12-29 22:32:41 +0700

    fix: minor lagi lupa

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -18,9 +18,8 @@
         </div>
         <div class="showcase-content">
             <h1>Predict & Prevent</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book.</p>
+            <p>Welcome to a new era of proactive healthcare at PulseGuard! Our innovative website is designed to empower
+                you with the tools to predict and prevent health concerns before they become major issues.</p>
             <a href="#" class="start-button">Read More</a>
         </div>
     </header>
